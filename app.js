0050eb99f486ca1164024bbee2ba3c59fdd343d0
Thomas Koelman <thomaskoelman@hotmail.com>
2024-06-21 14:24:02 +0200

    create board
    Add blank cards on the screen.

--- app.js
+++ app.js
@@ -23,3 +23,18 @@ const cardArray = [
     structuredClone(pizza),
     structuredClone(pizza)
 ]
+
+cardArray.sort(() => 0.5 - Math.random())
+
+const gridDisplay = document.querySelector('#grid')
+
+function createBoard() {
+    for (let i = 0; i < cardArray.length; i++) {
+        const card = document.createElement('img')
+        card.setAttribute('src', 'images/blank.png')
+        card.setAttribute('data-id', i)
+        gridDisplay.appendChild(card)
+    }
+}
+
+createBoard()
